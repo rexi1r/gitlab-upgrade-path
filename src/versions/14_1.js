27972a437b4783bd8824e88cba9b3301c169c6bb
Davin Walker <dwalker@gitlab.com>
2022-02-23 23:24:12 +0000

    Delete 14_1.js

--- src/versions/14_1.js
+++
@@ -1,8 +0,0 @@
-import flux from "@aust/react-flux";
-
-flux.dispatch("version/add", {
-  major: 14,
-  minor: 1,
-  comments:
-    "Note: 14.1 is not required when [fully updated on 14.0.x](https://docs.gitlab.com/ee/update/#1410)",
-});
