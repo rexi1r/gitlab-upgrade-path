d192a6b491bdadae85d1d4ddf2da9f846555921b
Davin Walker <dwalker@gitlab.com>
2022-08-02 12:04:24 -0600

    reduce total selection options for older versions; filter / uniq

--- ruby/collect_versions.rb
+++ ruby/collect_versions.rb
@@ -1,39 +1,54 @@
 #!/usr/bin/env ruby
 
+# rubocop:disable Lint/Void, Style/Semicolon
+
 # ============================================================
 # Helper to Create JS array of all versions
 # ============================================================
-require "labclient"
-require "sem_version"
+require 'labclient'
+require 'sem_version'
 
-client = LabClient::Client.new(url: "https://gitlab.com", token: "")
-all = client.projects.show("gitlab-org/gitlab").tags(search: "-ee$", per_page: 100).auto_paginate; :ok # 13s
+client = LabClient::Client.new(url: 'https://gitlab.com', token: '')
+all = client.projects.show('gitlab-org/gitlab').tags(search: '-ee$', per_page: 100).auto_paginate; :ok # 13s
 all.select! { |x| x.name =~ /^v(\d+)\.(\d+)\.(\d+)-ee$/ }; :ok # Filter by Valid Version
 
-all.map! { |x| SemVersion.new x.name.scan(/^v(.*)-ee$/).flatten.first }; :ok # Convert to SemVer
+# Format / Uniq Only
+all.map! { |x| x.name.scan(/^v(.*)-ee$/).flatten.first }.uniq!
 
-# 12 or Higher
-all.select! { |x| x.major >= 12 }
+# Convert to SemVer
+all.map! { |x| SemVersion.new x }; :ok
+
+# 8 or Higher
+all.select! { |x| x.major >= 8 }
 
 # List of all Versions
 index = all.sort.reverse.uniq.map do |version|
   { version: version.to_s, major: version.major, minor: version.minor }
 end
 
-targets = index.each_with_object({}) do |version, targets|
-  idx = version.slice(:major, :minor).values.join(".")
-  targets[idx] ||= version # Default to current
+targets = index.each_with_object({}) do |version, targets_build|
+  idx = version.slice(:major, :minor).values.join('.')
+  targets_build[idx] ||= version # Default to current
 
-  target = SemVersion.new targets.dig(idx, :version)  # Easier Access
+  target = SemVersion.new targets_build.dig(idx, :version) # Easier Access
 
-  targets[idx] = version if target < SemVersion.new(version[:version])
+  targets_build[idx] = version if target < SemVersion.new(version[:version])
+end
+
+# Filter Patches below 13
+index.select! do |version|
+  if version[:major] > 13
+    true
+  else
+    targets["#{version[:major]}.#{version[:minor]}"] == version
+  end
 end
 
 # Minor Version Targets
 output = {
   index: index,
-  targets: targets.values,
+  targets: targets.values
 }
 
 # src/util/all
-File.write("all.js", "const versions = #{output.to_json}\nexport default versions;")
+File.write('all.js', "const versions = #{output.to_json}\nexport default versions;")
